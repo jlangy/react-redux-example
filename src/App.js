c1c9506adf0ac00d8521c97de135fa7d444d6f2c
Jon Langlois <jonathan-langlois@live.ca>
2020-04-23 13:13:51 -0700

    tutorial setup sans redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from './components/Posts'
+import PostForm from './components/Postform'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PostForm />
+        <hr/>
+        <Posts />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Postform.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Postform extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      body: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form>
+          <div>
+            <label htmlFor="title-input">Title: </label>
+            <br/>
+            <input type="text" name="title" id="title-input" value={this.state.title} onChange={this.onChange}/>
+          </div>
+          <div>
+            <label htmlFor="body-input">Body: </label>
+            <br/>
+            <textarea name="body" id="body-input" value={this.state.body} onChange={this.onChange}/>
+          </div>
+          <br/>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Postform;
