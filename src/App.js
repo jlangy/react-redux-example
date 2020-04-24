055215a783dfa536aabdd4f712c17c26ead7d364
Jon Langlois <jonathan-langlois@live.ca>
2020-04-24 15:02:59 -0700

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts'
 import PostForm from './components/Postform'
+import { Provider } from 'react-redux'; 
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <PostForm />
-        <hr/>
-        <Posts />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <PostForm />
+          <hr/>
+          <Posts />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
