d30c23d1294ce4f579b9e2694ee1b41e69c11c83
Jon Langlois <jonathan-langlois@live.ca>
2020-04-25 16:57:36 -0700

    complete tutorial working redux app example

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createPost } from '../actions/postActions'
 
 class Postform extends Component {
   constructor(props){
@@ -24,16 +27,8 @@ class Postform extends Component {
       body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
+    this.props.createPost(post);
+
   }
   
   render() {
@@ -59,4 +54,8 @@ class Postform extends Component {
   }
 }
 
-export default Postform;
+Postform.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, {createPost })(Postform);
